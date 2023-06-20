5d8c30002590200a755b8810d9ebd94b7d29a087
leonardo sandoval aya <leonardoaya1234@gmail.com>
2023-06-20 14:25:23 -0500

    proyectos del portafolio

--- src/Details.js
+++ src/Details.js
@@ -73,6 +73,43 @@ export const techStackDetails = {
 // Enter your Project Details here
 export const projectDetails = [
   {
+    title: "Projecto 02  Page loading  Error 404",
+    image: projectImage5,
+    description: `Una página 404 responsive y muy con un diseño moderno y atractivo, 
+    con colores armoniosos y elementos visuales llamativos. Además, se adapta perfectamente a diferentes dispositivos, 
+    como teléfonos móviles y tablets, garantizando una experiencia de usuario óptima. Incluye también mensajes 
+    amigables y creativos que indican que la página buscada no se encuentra, invitando al usuario a explorar otras
+     secciones o a volver a la página principal.
+    `,
+    techstack: "JavaScript|Html5|Css3",
+    previewLink: "https://6491fb22bb61d61d7f22ee9d--golden-narwhal-049f8a.netlify.app/",
+    githubLink: "https://github.com/leonardo-sandoval/loadingpage404error.git",
+  },
+  {
+    title: "Projecto 01 Sticky Notes App",
+    image: projectImage4,
+    description: ` Aplicación  en la que puedes crear notas adhesivas digitales
+     de manera rápida y sencilla. Organiza tus tareas, recordatorios y pensamientos en pocos clics, 
+     y mantén tus ideas al alcance de tu mano en todo momento Con funciones de personalización y sincronización
+      en la nube, nunca perderás tus notas importantes
+    `,
+    techstack: "JavaScript|Html5|Css3",
+    previewLink: "https://6490c7782348aa39edce5f62--chimerical-hamster-0f6a93.netlify.app/",
+    githubLink: "https://github.com/leonardo-sandoval/StickyNotesApp",
+  },
+  {
+    title: "Projecto App clima",
+    image: projectImage3,
+    description: ` aplicación de clima basada en JavaScript que consume una API para brindar 
+    a los usuarios información actualizada y precisa sobre las condiciones meteorológicas en tiempo real.
+     Con una interfaz intuitiva, 
+    los usuarios pueden obtener fácilmente el pronóstico del clima de su ubicación o de cualquier otra ciudad.
+    `,
+    techstack: "JavaScript|Html5|Css3",
+    previewLink: "https://6490a093781df819d725ef3f--magenta-palmier-375cde.netlify.app/",
+    githubLink: "https://github.com/leonardo-sandoval/APPCLIMA-API",
+  },
+  {
     title: "Projecto TaskCalendar",
     image: projectImage1,
     description: ` Es una herramienta indispensable para la gestión del tiempo.
@@ -82,6 +119,16 @@ export const projectDetails = [
     techstack: "JavaScript|PHP|Mysql|Bootstrap",
     previewLink: "https://calendario-production-b6a3.up.railway.app/",
     githubLink: "https://github.com/leonardo-sandoval/CALENDARIO",
+  },
+  {
+    title: "Projecto Tienda virtual Unlimited",
+    image: projectImage2,
+    description: ` El proyecto de la tienda online es una solución de 
+    comercio electrónico de código abierto escrita en PHP y basada en MySQL. 
+    Con su enfoque en brindar una experiencia de carrito de compras . 
+    `,
+    techstack: "Html5|Css3|PHP|Mysql|Bootstrap|",
+    githubLink: "https://github.com/leonardo-sandoval/proyecto-unlimited-",
   },
 
 ];
